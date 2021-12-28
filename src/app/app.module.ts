145961aaafab29f524f8d6023f126bd2092413b2
soRodriguezz <s.rodriguezzapata01@gmail.com>
2021-12-28 20:27:54 -0300

    create docs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './shared/menu/menu.component';
 
+import {APP_BASE_HREF} from '@angular/common';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +16,7 @@ import { MenuComponent } from './shared/menu/menu.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [{provide: APP_BASE_HREF, useValue: '/mapas-app-angular/app'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
